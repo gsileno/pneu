d4fba07e8f7c4b8459fe11ee38681752b1c079f4
s1l3n0 <g.sileno@uva.nl>
2015-02-05 22:33:51 +0100

    trying a strategy from absolute to relative positions in tikz

--- src/org/leibnizcenter/pneu/graphics/export/PN2LaTeX.groovy
+++ src/org/leibnizcenter/pneu/graphics/export/PN2LaTeX.groovy
@@ -1,10 +1,14 @@
 package org.leibnizcenter.pneu.graphics.export
 
 import org.leibnizcenter.pneu.components.Net
+import org.leibnizcenter.pneu.components.Place
 
 /* See on http://www.texample.net/tikz/examples/nodetutorial/ for the preamble */
 
+
 class PN2LaTeX {
+
+    static enum Cardinality { NORTH, EAST, SOUTH, WEST }
 
     static convert(Net net) {
         String code = ""
@@ -13,10 +17,101 @@ class PN2LaTeX {
 
         code += "  \\begin{scope}\n"
 
-        net.placeList.each() { pl ->
+        for (int i = 0; i < net.placeList.size(); i++) {
+            Place pl = net.placeList.get(i)
+
             code += "    \\node\t"
             code += "[place]\t"
             code += "("+pl.id+")\t"
+
+            println "new place @@@@@@@@"
+
+            // find the nearest from the remaining nodes
+            Place proxiestx, proxiesty
+            Integer minx, miny
+            Cardinality dirx, diry
+            Integer dx, dy
+
+            if (i > 0) {
+
+                for (int j = i - 1; j >= 0; j--) {
+                    dx = dy = null
+
+                    Place proxy = net.placeList.get(j)
+
+                    if (proxy.id != pl.id) {
+                        dx = proxy.position.x - pl.position.x
+                        dy = proxy.position.y - pl.position.y
+
+                        println "minX: "+minx +" - dX: "+dx
+                        println "minY: "+miny +" - dY: "+dy
+
+                        if (minx == null) minx = dx.abs()
+                        else if (minx != 0) {
+                            if (dx.abs() < minx) {
+                                minx = dx.abs()
+                                println "X: "+minx +": "+dx
+                                if (dx > 0) {
+                                    dirx = Cardinality.EAST
+                                    proxiestx = proxy
+                                } else if (dx < 0) {
+                                    dirx = Cardinality.WEST
+                                    proxiestx = proxy
+                                } else {
+                                    dirx = null
+                                    proxiestx = null
+                                }
+                            }
+                        }
+
+                        if (miny == null) miny = dy.abs()
+                        else if (miny != 0) {
+                            if (dy.abs() < miny) {
+                                miny = dy.abs()
+                                println "Y: "+miny +": "+dy
+                                if (dy > 0) {
+                                    diry = Cardinality.NORTH
+                                    proxiesty = proxy
+                                } else if (dy < 0) {
+                                    diry = Cardinality.SOUTH
+                                    proxiesty = proxy
+                                } else {
+                                    diry = null
+                                    proxiesty = null
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (pl.name.trim() == "") pl.name = null
+
+            if (dirx != null || diry != null || pl.name != null) {
+                code += "["
+
+                if (dirx != null) {
+                    if (dirx == Cardinality.EAST) code += "left of="
+                    if (dirx == Cardinality.WEST) code += "right of="
+                    code += proxiestx.id
+                    if (diry != null || pl.name != null) { code +="," }
+                }
+
+                if (diry != null) {
+                    if (diry == Cardinality.NORTH) code += "above of="
+                    if (diry == Cardinality.SOUTH) code += "below of="
+                    code += proxiesty.id
+                    if (pl.name != null) { code +="," }
+                }
+
+                if (pl.name != null) {
+                    code += "label="+pl.name.trim()
+                }
+
+                code += "]\t"
+            }
+
             code += "{};\n"
         }
 
